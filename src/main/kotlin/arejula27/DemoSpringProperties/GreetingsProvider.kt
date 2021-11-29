36da32d7269b0412a696d242af7f70bac31d47dc
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-11-29 15:28:56 +0100

    no va

--- src/main/kotlin/arejula27/DemoSpringProperties/GreetingsProvider.kt
+++ src/main/kotlin/arejula27/DemoSpringProperties/GreetingsProvider.kt
@@ -1,16 +1,19 @@
 package arejula27.DemoSpringProperties
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
 import java.util.*
 
 interface GreetingsProvider {
     fun Greet(): String
 }
 
+@PropertySource
 class GreetingsProviderImpl :GreetingsProvider{
 
+    @Value("\${my.greet}")
     lateinit var  greet: String
 
 
-    override fun Greet(): String = AppConfiguration.greet
+    override fun Greet(): String = greet
 }
